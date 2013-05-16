eb0fdaab68917e6ec07dc62cc7c083428829f38c
Raul Mera <rmera@chemdothelsinki.fi>
2013-05-16 17:14:53 +0300

    just added gocoords

--- gonum.go
+++ gonum.go
@@ -40,29 +40,171 @@ import "github.com/skelterjohn/go.matrix"
  
  
  
-type gnFloat64 matrix.DenseMatrix
+type CoordMatrix matrix.DenseMatrix
  
  
- func gnNewFloat64(data []float64,rows,cols int) *gnFloat64{
-	return gnFloat64(matrix.MakeDenseMatrix(data, rows, cols))
+func NewCoordMatrix(data []float64,rows,cols int) *CoordMatrix{
+	return CoordSet(matrix.MakeDenseMatrix(data, rows, cols))
 }
   
+func (F *CoordMatrix)  Norm(i int)(float64){  
+	//temporary hack
+	if i != 2 {
+		panic("only 2-norm is implemented")
+		}
+	return F.TwoNorm()
+}
+ 
 
-(F *gnFloat64) Dims()(int, int){
+func (F *CoordMatrix)  Clone(A *CoordMatrix){
+	ar,ac:=A.Dims()
+	if F==nil{
+		F:=A.Copy()
+	}else{
+		fr,fc:=F.Dims()
+		if ac != fc || ar != fr {
+			panic("receiver must has same shape as to-be-cloned matrix")
+		}
+	}
+	F.SetMatrix(A)
+	 
+}
+
+
+func (F *CoordMatrix)  Dims()(int, int){
 	return F.Rows(),F.Cols()
 }
 
+func (F *CoordMatrix)  At(A, B int)(float64){
+	return F.Get(A,B)
+}
+
+func (F *CoordMatrix)  ElemMul(A, B *CoordMatrix) {
+	arows,acols:=A.Dims()
+	brows,bcols:=B.Dims()
+	if arows != brows || acols != bcols{
+		panic("Matrices need to have the same shape for ElemMul")
+		}
+		for i=0;i<arows;i++{
+			for j=0;j<acols;j++ {
+				F.Set(i,j,A.At(i,j)*B.At(i,j))
+				}
+			
+			}
+	}
+
+
+//Sum returns the sum of all elements in matrix A.
+func (F *CoordMatrix)  Sum()  float64 {
+	Rows,Cols:=F.Dims()
+	var sum float64
+	for i := 0; i < Cols; i++ {
+		for j := 0; j < Rows; j++ {
+			sum += A.Get(j, i)
+		}
+	}
+	return sum
+}
+
+ 
+func (F *CoordMatrix)  Mul(A, B *CoordMatrix) {
+	if A.Cols() != B.Rows() {
+		panic("Wrong dimenstions for matrix multiplication")
+	}
+	Arows,Acols:=A.Dims()
+	Brows,Bcols:=B.Dims()
+	
+	if F==nil{
+	F = Zeros(Arows, Bcols) //I don't know if the final API will allow this.
+	}
+	
+	in := make(chan int)
+	quit := make(chan bool)
+
+	dotRowCol := func() {
+		for {
+			select {
+			case i := <-in:
+				sums := make([]float64, Bcols)
+				for k := 0; k < Acols; k++ {
+					for j := 0; j < Bcols; j++ {
+						sums[j] += A.At(i, k) * B.At(k, j)
+					}
+				}
+				for j := 0; j < Bcols; j++ {
+					F.Set(i, j, sums[j])
+				}
+			case <-quit:
+				return
+			}
+		}
+	}
+
+	threads := 2
+
+	for i := 0; i < threads; i++ {
+		go dotRowCol()
+	}
+
+	for i := 0; i < Arows; i++ {
+		in <- i
+	}
+
+	for i := 0; i < threads; i++ {
+		quit <- true
+	}
+
+	return
+}
  
  
+func (F *CoordMatrix) Stack(A,B *CoordMatrix)  {
+	Arows,Acols:=A.Dims()
+	Brows,Bcols:=B.Dims()
+	
+	if Acols != Bcols {
+		panic("Matrices for stacking don't have the same columns")
+	}
+
+	if F==nil{
+	F = Zeros(Arows+Brows, Acols) //I don't know if the final API will allow this.
+	}
+
+	F = Zeros(Arows+Brows, Acols)
+	err = A.StackFill(B, F)
+	return
+}
  
+
+//Dot returns the dot product between 2 vectors or matrices
+func (F *CoordMatrix) Dot(B *CoordMatrix) float64 {
+	var err error
+	if F.Cols() != B.Cols() || F.Rows() != B.Rows() {
+		panic("Dot: Matrices must have the same dimmension to obtain dot product")
+	}
+	a,b:=F.Dims()
+	A := Zeros(a,b)
+	A.ElemMul(F,B)
+	if err != nil {
+		panic(err.Error())
+	}
+	return F.Sum()
+}
  
- 
- 
- 
- 
- 
- 
- 
+//Transpose 
+func (F *CoordMatrix) T(A *CoordMatrix)  {
+	arows,acols:=A.Dims()
+	frows,fcols:=F.Dims()
+	if arows != fcols || acols != frows{
+		panic("Mismatched matrices for Transposing")
+	}
+	for i=0;i<arows;i++{
+		for j=0;j<cols;j++ {
+			F.Set(i,j,A.At(j,i))
+		}
+		
+	}
+} 
  
  
  
--- matrix.go
+++ matrix.go
@@ -40,109 +40,10 @@ import "fmt"
 //Some of this functions don't return error messages because they are meant to
 //Be inserted in mathematical expressions and thus they need to return only one value.
 
-/*SetRows replaces the coordinates of atoms in the positions given by atomlist with the coordinates
-in newcoords (in order) If atomlist contains a single element, it replaces as many coordinates
-as given in newcoords, starting 
-at the element in atomlist. In the latter case, the function checks that there are enough coordinates to
-replace and panics if not. */
-func SetRows(target, newcoords *matrix.DenseMatrix, atomlist []int) *matrix.DenseMatrix {
-	//If supplies a list with one number, the newcoords will replace the old coords
-	//Starting that number. We do check that you don't put more coords than spaces we have.
-	returned := target.Copy()
-	if len(atomlist) == 1 {
-		if newcoords.Rows() > target.Rows()-atomlist[0]-1 {
-			panic(fmt.Sprintf("Cant replace starting from position %d: Not enough atoms in molecule", atomlist[0]))
-		}
-		returned.SetMatrix(atomlist[0], 0, newcoords)
-		return returned
-	}
-	//If the list has more than one atom
-	lenatoms := target.Rows()
-	for k, j := range atomlist {
-		if j > lenatoms-1 {
-			panic(fmt.Sprintf("Requested position number: %d (%d) out of range", k, j))
-		}
-		returned.SetMatrix(j, 0, newcoords.GetRowVector(k))
-	}
-	return returned
-}
 
-//Somerows, given a list of ints and the desired frame, returns an slice matrix.DenseMatrix
-//containing the coordinates of the atoms with the corresponding index.
-//This function returns a copy, not a reference, so changes to the returned matrix
-//don't alter the original. It check for correctness of the frame and the
-//Atoms requested.
-func SomeRows(M *matrix.DenseMatrix, clist []int) *matrix.DenseMatrix {
-	rows := M.Rows()
-	ret := make([][]float64, 0, len(clist))
-	for k, j := range clist {
-		if j > rows-1 {
-			panic(fmt.Sprintf("Coordinate requested (Number: %d, value: %d) out of range!", k, j))
-		}
-		tmp := M.GetRowVector(j).Array()
-		if len(tmp) != 3 {
-			panic(fmt.Sprintf("Coordinate %d has %d components instead of 3", k, len(tmp)))
-		}
-		ret = append(ret, tmp)
-	}
-	return matrix.MakeDenseMatrixStacked(ret)
-}
 
-//SafeSomeRows, given a list of ints and the desired frame, returns an slice matrix.DenseMatrix
-//containing the coordinates of the atoms with the corresponding index.
-//This function returns a copy, not a reference, so changes to the returned matrix
-//don't alter the original. It check for correctness of the frame and the
-//Atoms requested. Unlike SomeRow, which panics in these cases, SafeSomeRows returns an error. if problems,
-//or nil.
-func SafeSomeRows(M *matrix.DenseMatrix, clist []int) (*matrix.DenseMatrix, error) {
-	rows := M.Rows()
-	ret := make([][]float64, 0, len(clist))
-	for k, j := range clist {
-		if j > rows-1 {
-			return nil, fmt.Errorf("Coordinate requested (Number: %d, value: %d) out of range!", k, j)
-		}
-		tmp := M.GetRowVector(j).Array()
-		if len(tmp) != 3 {
-			return nil, fmt.Errorf("Coordinate %d has %d components instead of 3", k, len(tmp))
-		}
-		ret = append(ret, tmp)
-	}
-	return matrix.MakeDenseMatrixStacked(ret), nil
-}
 
-//Unitarize takes a vector and divides it by its norm
-//thus obtaining an unitary vector pointing in the same direction as 
-//vector.
-func Unitarize(vector *matrix.DenseMatrix) *matrix.DenseMatrix {
-	norm := vector.TwoNorm()
-	norm = 1.0 / norm
-	F := vector.Copy()
-	F.Scale(norm)
-	return F
-}
 
-//AddRow adds the row vector row to each row of the matrix big, in place. Both need the same ammount of columns.
-func AddRow(big, row *matrix.DenseMatrix) error {
-	bigrows := big.Rows()
-	if big.Cols() != row.Cols() || row.Rows() != 1 {
-		return fmt.Errorf("Ill-formed matrices for multiplication")
-	}
-	for i := 0; i < bigrows; i++ {
-		j := big.GetRowVector(i)
-		j.Add(row)
-	}
-	return nil
-}
-
-//SubRow substract the row bector row from each row of the matrix big, in place. Bothe ned the same ammount 
-//of coulmn. This is not an efficient function since it contains two function calls, one to the DenseMatrix
-//method Scale() and other to the function AddRow().
-func SubRow(big, row *matrix.DenseMatrix) error {
-	row2 := row.Copy()
-	row2.Scale(-1)
-	err := AddRow(big, row2)
-	return err //nil if no error, something else otherwise
-}
 
 //Cross3D Takes 2 3-len column or row vectors and returns a column or a row
 //vector, respectively, with the Cross product of them.
@@ -197,46 +98,8 @@ func KronekerDelta(a, b float64) float64 {
 	return 0
 }
 
-//Dot returns the dot product between 2 vectors or matrices. Just the sum of the 
-//Element-wise multiplication. In this case returning error
-//makes it problematic to use in complex operations, so this returns -1
-//when problems.
-func Dot(A, B *matrix.DenseMatrix) float64 {
-	var err error
-	if A.Cols() != B.Cols() || A.Rows() != B.Rows() {
-		panic("Dot: Matrices must have the same dimmension to obtain dot product")
-	}
-	//For some crazy reason if the F variable is called C, I get a
-	//"ScaleMatrixDense undeclared" error at compile time :S
-	F := A.Copy()
-	err = F.ScaleMatrixDense(B)
-	if err != nil {
-		panic(err.Error())
-	}
-	return DMSummation(F)
-}
 
-//DMPowInPlace raises the DenseMatrix A, element-wise, to the nth power.  
-func DMPowInPlace(A *matrix.DenseMatrix, n float64) {
-	for i := 0; i < A.Rows(); i++ {
-		for j := 0; j < A.Cols(); j++ {
-			//	fmt.Println(i,j, A.Rows(),A.Cols())
-			A.Set(i, j, math.Pow(A.Get(i, j), n))
-		}
-	}
-}
 
-//DMPow returns B^n. It does not modify B.
-func DMPow(B *matrix.DenseMatrix, n float64) *matrix.DenseMatrix {
-	A := B.Copy()
-	for i := 0; i < A.Rows(); i++ {
-		for j := 0; j < A.Cols(); j++ {
-			//	fmt.Println(i,j, A.Rows(),A.Cols())
-			A.Set(i, j, math.Pow(A.Get(i, j), n))
-		}
-	}
-	return A
-}
 
 //DMScaleByCol scales each column of matrix A by Col.
 func DMScaleByCol(A, Col *matrix.DenseMatrix) error {
@@ -265,18 +128,6 @@ func DMScaleByRow(A, Row *matrix.DenseMatrix) error {
 	return nil
 }
 
-//DMSummation returns the sum of all elements in matrix A.
-func DMSummation(A *matrix.DenseMatrix) float64 {
-	Rows := A.Rows()
-	Cols := A.Cols()
-	var sum float64
-	for i := 0; i < Cols; i++ {
-		for j := 0; j < Rows; j++ {
-			sum += A.Get(j, i)
-		}
-	}
-	return sum
-}
 
 //DMaddfloat returns a matrix which elements are those of matrix A plus the float B.
 func DMaddfloat(A *matrix.DenseMatrix, B float64) *matrix.DenseMatrix {
