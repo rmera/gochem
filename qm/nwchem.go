a8e35d87df3437e417a24f39f8b4e4c2c7008b3a
rmera <rmeraa@gmail.com>
2013-12-02 22:17:32 +0100

    fixed typo in nwchem

--- qm/nwchem.go
+++ qm/nwchem.go
@@ -167,7 +167,7 @@ func (O *NWChemHandle) BuildInput(coords *chem.VecMatrix, atoms chem.ReadRef, Q 
 	if Q.SCFConvHelp > 0 {
 		scfiters = "iterations 200"
 		if  Q.Guess==""{
-			prevscf="\nbasis\"ao basis\"\n * library 3-21g\nend\nscf\n maxiter 200\n vectors output hf.movecs\nend\ntask scf energy\n\n"
+			prevscf="\nbasis \"ao basis\"\n * library 3-21g\nend\nscf\n maxiter 200\n vectors output hf.movecs\nend\ntask scf energy\n\n"
 			vectors=fmt.Sprintf("vectors input hf.movecs output %s.movecs",O.inputname)
 		}
 	}
